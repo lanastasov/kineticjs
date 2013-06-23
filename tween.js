e49fb759101da80cdf6a0de5fff7d502a946e374
lanastasov <lubomir.anastasov@gmail.com>
2013-06-23 22:43:17 +0300

    tween line and rectangle

--- tween.js
+++ tween.js
@@ -6,6 +6,8 @@ var stage = new Kinetic.Stage({
 	width	 : 1024,
 	height	 : 2000
 });
+var layer = new Kinetic.Layer();
+
 
 var line0 = new Kinetic.Line({
 	x: 100,
@@ -23,9 +25,9 @@ var line1 = new Kinetic.Line({
 });
 
 var line2 = new Kinetic.Line({
-	x : 100,
-	y : 50,
-	points : [100, 200, 300, 400],
+	x : 300,
+	y : 300,
+	points : [100, 100, 300, 300],
 	stroke: 'green',
 	dashArray: [66, 5],
 	shadowColor: 'black',
@@ -41,15 +43,15 @@ var line2 = new Kinetic.Line({
 	y: 100,
 	width: 200,
 	height: 50,
-	fill: 'green',
-	stroke: 'black',
+	fill: 'yellow',
+	stroke: 'red',
 	strokeWidth: 2,
 	opacity: 0.2
-  });
+});
 
 
 // var tween = new Kinetic.Tween({
-	// node: line2,
+	// node: rect,
 	// duration: 1,
 	// x: 300,
 	// y: 300,
@@ -59,16 +61,38 @@ var line2 = new Kinetic.Line({
 	// scaleX: 1.5
 // });
 
+layer.add(rect);
+layer.add(line2);
+//line2.setOffset(100, 100);
+var tween = new Kinetic.Tween({
+	node: line2, 
+	duration: 10	,
+	x: 0,
+	y: 0,
+	fill: 'red',
+	rotation: Math.PI*2,
+	opacity: 1,
+	strokeWidth: 1,
+	scaleX: 1,
+	shadowColor: 'black',
+	shadowBlur: 20,
+	shadowOffset: 2,
+	shadowOpacity: 135,
+	draggable: true,
+	strokeWidth: 5
+});
+
 
 rect.on('click', function() {
+	console.log(this);
 	tween.play();
 });
 
-line2.on('click', function(){
-	console.log(this);
-});
+// line2.on('click', function() {
+	// tween.play();
+	// console.log(this);
+// });
 
-var layer = new Kinetic.Layer();
 layer.add(line0);
 layer.add(line1);
 layer.add(line2);
